fe263b1348ecd6044560cd89cd0c0021752a79c4
1gxv1 <928505117@qq.com>
2024-04-29 22:05:44 +0800

    refactor:重构短链接后台请求路径

--- admin/src/main/java/com/chr1s/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/chr1s/shortlink/admin/controller/GroupController.java
@@ -3,6 +3,7 @@ package com.chr1s.shortlink.admin.controller;
 import com.chr1s.shortlink.admin.common.convention.result.Result;
 import com.chr1s.shortlink.admin.common.convention.result.Results;
 import com.chr1s.shortlink.admin.dto.req.GroupSaveReqDTO;
+import com.chr1s.shortlink.admin.dto.req.GroupSortReqDTO;
 import com.chr1s.shortlink.admin.dto.req.GroupUpdateReqDTO;
 import com.chr1s.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.chr1s.shortlink.admin.service.GroupService;
@@ -16,20 +17,32 @@ import java.util.List;
 public class GroupController {
     private final GroupService groupService;
 
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> add(@RequestBody GroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getGroupName());
         return Results.success();
     }
 
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
         return Results.success(groupService.listGroup());
     }
 
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/api/short-link/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody GroupUpdateReqDTO requestParam) {
         groupService.updateGroup(requestParam);
+        return Results.success();
+    }
+
+    @DeleteMapping("/api/short-link/admin/v1/group")
+    public Result<Void> updateGroup(@RequestParam String gid) {
+        groupService.deleteGroup(gid);
+        return Results.success();
+    }
+
+    @PostMapping("/api/short-link-v1/admin/sort/group")
+    public Result<Void> sortGroup(@RequestBody List<GroupSortReqDTO> requestParam) {
+        groupService.sortGroup(requestParam);
         return Results.success();
     }
 }
--- admin/src/main/java/com/chr1s/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/chr1s/shortlink/admin/controller/UserController.java
@@ -17,29 +17,29 @@ public class UserController {
 
     private final UserService userService;
 
-    @GetMapping("/api/short-link/v1/user/{username}")
+    @GetMapping("/api/short-link/admin/v1/user/{username}")
     public Result<UserRespDTO> getUserByUsername(@PathVariable("username") String username) {
         return Results.success(userService.getUserByUserName(username));
     }
 
-    @GetMapping("/api/short-link/v1/user/exist/{username}")
+    @GetMapping("/api/short-link/admin/v1/user/exist/{username}")
     public Result<Boolean> isExistUser(@PathVariable("username") String username) {
         return Results.success(userService.isExistUser(username));
     }
 
-    @PostMapping("/api/short-link/v1/user/register")
+    @PostMapping("/api/short-link/admin/v1/user/register")
     public Result<Void> register(@RequestBody UserRegisterReqDTO requestParam) {
         userService.register(requestParam);
         return Results.success();
     }
 
-    @PutMapping("/api/short-link/v1/user/update")
+    @PutMapping("/api/short-link/admin/v1/user/update")
     public Result<Void> update(@RequestBody UserUpdateReqDTO requestParam) {
         userService.update(requestParam);
         return Results.success();
     }
 
-    @PostMapping("/api/short-link/v1/user/login")
+    @PostMapping("/api/short-link/admin/v1/user/login")
     public Result<UserLoginRespDTO> update(@RequestBody UserLoginReqDTO requestParam) {
         return Results.success(userService.login(requestParam));
     }
