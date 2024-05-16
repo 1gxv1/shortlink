1f9746ab537ed6f7db3fae2306ab6092d7d6cc39
1gxv1 <928505117@qq.com>
2024-05-16 17:56:52 +0800

    feature：增加短链接错误状态码

---
+++ project/src/main/java/com/chr1s/shortlink/project/common/enums/UserErrorCodeEnum.java
@@ -0,0 +1,32 @@
+package com.chr1s.shortlink.project.common.enums;
+
+import com.chr1s.shortlink.admin.common.convention.errorCode.IErrorCode;
+
+public enum UserErrorCodeEnum implements IErrorCode {
+    UserNull("B000200", "用户记录不存在"),
+
+    USER_EXIST("B000202", "用户名重复"),
+
+    USER_SAVE_ERROR("B000203", "用户插入失败"),
+
+    USER_UPDATE_ERROR("B000204", "用户更新信息失败");
+
+    private final String code;
+
+    private final String message;
+
+    UserErrorCodeEnum(String code, String message) {
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public String code() {
+        return code;
+    }
+
+    @Override
+    public String message() {
+        return message;
+    }
+}
