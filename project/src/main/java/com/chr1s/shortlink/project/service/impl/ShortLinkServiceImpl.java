08fcbefeb5dac1eba8c3eae790cbc38648802222
1gxv1 <928505117@qq.com>
2024-06-19 20:57:11 +0800

    fix:修复短链接域名布隆过滤器问题

--- project/src/main/java/com/chr1s/shortlink/project/service/impl/ShortLinkServiceImpl.java
+++ project/src/main/java/com/chr1s/shortlink/project/service/impl/ShortLinkServiceImpl.java
@@ -659,7 +659,7 @@ public class ShortLinkServiceImpl extends ServiceImpl<ShortLinkMapper, ShortLink
             String originUrl = requestParam.getOriginUrl();
             originUrl += UUID.randomUUID().toString();
             shortUri = HashUtil.hashToBase62(originUrl);
-            if (!shortUriCreateCachePenetrationBloomFilter.contains(requestParam.getDomain() + "/" + shortUri)) {
+            if (!shortUriCreateCachePenetrationBloomFilter.contains(createShortLinkDefaultDomain+ "/" + shortUri)) {
                 break;
             }
             customGenerateCount++;
