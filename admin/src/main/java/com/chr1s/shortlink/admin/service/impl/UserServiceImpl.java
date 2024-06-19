2cfa287223a515223538164b4543eb55b4c812ef
1gxv1 <928505117@qq.com>
2024-06-19 20:46:34 +0800

    fix:修复短链接单一用户在不同地方同时登录

--- admin/src/main/java/com/chr1s/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/chr1s/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.chr1s.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.alibaba.fastjson2.JSON;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
@@ -27,6 +28,7 @@ import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
 import java.util.UUID;
 
 import static com.chr1s.shortlink.admin.common.constant.RedisCacheConstant.USER_LOGIN_KEY;
@@ -97,9 +99,20 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDo> implements 
                 .eq(UserDo::getPassword, requestParam.getPassword());
         UserDo userDo = baseMapper.selectOne(eq);
         if (userDo == null) throw new ClientException("数据库访问失败，可能是用户名或者密码错误");
+
+        Map<Object, Object> hasLoginMap = stringRedisTemplate.opsForHash().entries("login_" + requestParam.getUsername());
+        if (CollUtil.isNotEmpty(hasLoginMap)) {
+            String token = hasLoginMap
+                    .keySet()
+                    .stream()
+                    .findFirst()
+                    .map(Object::toString)
+                    .orElseThrow(() -> new ClientException("用户登录错误！"));
+            return new UserLoginRespDTO(token);
+        }
+
         String s = UUID.randomUUID().toString().replaceAll("-", "");
-        String key = USER_LOGIN_KEY + s;
-        stringRedisTemplate.opsForHash().put(key, "user",JSON.toJSONString(userDo));
+        stringRedisTemplate.opsForHash().put(USER_LOGIN_KEY + s, "user", JSON.toJSONString(userDo));
         return new UserLoginRespDTO(s);
     }
 }
